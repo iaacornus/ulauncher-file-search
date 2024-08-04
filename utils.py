a7c092b9db32bfcd58b3ba3a8c72af1a885ec5d9
James Aaron Erang <iaacornus.devel@gmail.com>
2024-08-04 09:43:39 +0800

    func for finding icon theme dir

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 from os import environ
+from os.path import exists
+
+
 def check_desktop_env() -> str:
     """
     Check the current desktop environment.
@@ -10,3 +13,27 @@ def check_desktop_env() -> str:
     desktop: str = environ.get("DESKTOP_SESSION") or ""
     return desktop.lower()
 
+def find_icons(HOME: str, icon_theme: str) -> list[str]:
+    """
+    Find the icons currently used by the system.
+
+    Parameters:
+        HOME: str - path of user home dir.
+        icon_theme: str - currently set icon theme.
+
+    Returns:
+        existing_dir: list[str] - the path of currently set icon theme.
+    """
+
+    ICON_DIR: list[str] = [
+            "/usr/share/icons",
+            f"{HOME}/.local/share/icons"
+            f"{HOME}/.icons",
+        ]
+    existing_dir: list[str] = []
+
+    for dir_ in ICON_DIR:
+        if exists(icon_theme_dir_ := f"{dir_}/{icon_theme}"):
+            existing_dir.append(icon_theme_dir_)
+
+    return existing_dir
